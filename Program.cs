dccfa0dd7fc7e84bd80557e66ede6deb565c8712
Åsa Berglund <71882921+asaber123@users.noreply.github.com>
2022-02-18 16:37:03 +0100

    added comments

--- Program.cs
+++ Program.cs
@@ -6,16 +6,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Adding database coonection to sqlite
 builder.Services.AddDbContext<SongContext>(options => 
     options.UseSqlite(builder.Configuration.GetConnectionString("SqliteString")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
